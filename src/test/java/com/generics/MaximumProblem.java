929b4a3a77cdf0438bdcc6130e9f367c2ba7a04b
Priyankateke <priiyankateke1705@gmail.com>
2020-04-10 02:58:24 -0700

    ADDED: Given Three Integer Number At Any Position Returns Max Number

--- src/test/java/com/generics/MaximumProblem.java
+++ src/test/java/com/generics/MaximumProblem.java
@@ -1,7 +1,17 @@
 package com.generics;
 
 public class MaximumProblem {
-    public static void main(String[] args) {
-        System.out.println("Welcome Maximum Problem Using Generics");
+    public Integer findMaximumNumber(Integer firstNumber, Integer secondNumber, Integer thirdNumber){
+        Integer maximum=firstNumber;
+
+        if(maximum.compareTo(secondNumber) < 0) {
+            maximum=secondNumber;
+        }
+
+        if(maximum.compareTo(thirdNumber) < 0) {
+            maximum=thirdNumber;
+        }
+
+        return maximum;
     }
 }
--- src/test/java/com/generics/MaximumProblemTest.java
+++ src/test/java/com/generics/MaximumProblemTest.java
@@ -1,5 +1,29 @@
 package com.generics;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class MaximumProblemTest {
     MaximumProblem maximumProblem=new MaximumProblem();
+
+    @Before
+    public void setUp() throws Exception {
+        maximumProblem=new MaximumProblem();
+    }
+
+    @Test
+    public void givenIntegerMaxNumber_WhenAtFirstPosition_ShouldReturnSameNumber() {
+        Assert.assertSame(50,maximumProblem.findMaximumNumber(50,20,30));
+    }
+
+    @Test
+    public void givenIntegerMaxNumber_WhenAtSecondPosition_ShouldReturnSameNumber() {
+        Assert.assertSame(50,maximumProblem.findMaximumNumber(20,50,30));
+    }
+
+    @Test
+    public void givenIntegerMaxNumber_WhenAtThirdPosition_ShouldReturnSameNumber() {
+        Assert.assertSame(50, maximumProblem.findMaximumNumber(20, 30, 50));
+    }
 }
